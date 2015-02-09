da1a4ea00edb94dfeaa584c1cd9f0f188db895ba
i7709331 <i7709331@14f4da34-90cd-4903-b01f-53c92ec1b671>
2015-02-09 09:45:09 +0000

    Added Ship Placement activity to segregate different play-phases.

---
+++ src/BoardViewShips.java
@@ -0,0 +1,15 @@
+import android.content.Context;
+import android.util.AttributeSet;
+import uk.co.bkrpage.battleshipsi7709331.BoardView;
+
+
+public class BoardViewShips extends BoardView {
+
+	public BoardViewShips(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/uk/co/bkrpage/battleshipsi7709331/Ship_Placement.java
@@ -0,0 +1,13 @@
+package uk.co.bkrpage.battleshipsi7709331;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ship_Placement extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ship_placement);
+	}
+}
