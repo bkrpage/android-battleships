7b6fa888a0b4f70de83378ddf2e7c70078f244c9
i7709331 <i7709331@14f4da34-90cd-4903-b01f-53c92ec1b671>
2015-02-12 16:42:56 +0000

    Added global variables for ship orientation in ships.java

--- BattleshipsI7709331/src/uk/co/bkrpage/battleshipsi7709331/Ship.java
+++ BattleshipsI7709331/src/uk/co/bkrpage/battleshipsi7709331/Ship.java
@@ -6,6 +6,10 @@ public class Ship {
 	private int col;
 	private int size = 1;
 	private boolean horiz = true;
+	
+	// These are final variables for the ship orientation.
+	public static final boolean HORIZONTAL = true;
+	public static final boolean VERTICAL = false;
 
 	public Ship(int x, int y, int size , boolean horiz){
 		x = this.row;
