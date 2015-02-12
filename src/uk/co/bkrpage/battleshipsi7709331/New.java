d1544d219c5d86efaa1fb46001965ead964580b3
i7709331 <i7709331@14f4da34-90cd-4903-b01f-53c92ec1b671>
2015-02-12 11:02:12 +0000

    Added battleship placement count and a starting button for moing to the next player.

---
+++ src/uk/co/bkrpage/battleshipsi7709331/New.java
@@ -0,0 +1,15 @@
+package uk.co.bkrpage.battleshipsi7709331;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class New extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new);
+	}
+}
