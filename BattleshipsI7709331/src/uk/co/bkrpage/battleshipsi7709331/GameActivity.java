93f1257b9ecfd757ffa5d6ef80be693a4bfabf3d
i7709331 <i7709331@14f4da34-90cd-4903-b01f-53c92ec1b671>
2015-02-24 18:04:30 +0000

    Removed warnings

--- BattleshipsI7709331/src/uk/co/bkrpage/battleshipsi7709331/GameActivity.java
+++ BattleshipsI7709331/src/uk/co/bkrpage/battleshipsi7709331/GameActivity.java
@@ -1,11 +1,7 @@
 package uk.co.bkrpage.battleshipsi7709331;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class GameActivity extends Activity {
 
